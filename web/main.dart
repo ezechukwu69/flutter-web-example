0ab24010d82af1ce0063b19b85f5491063333f9f
ezechukwu69 <62510314+ezechukwu69@users.noreply.github.com>
2020-04-14 19:27:45 +0100

    Changes to flutter_web

--- web/main.dart
+++ web/main.dart
@@ -1,7 +1,7 @@
 import 'dart:html';
-import 'package:flutter_web/material.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_web_example/main.dart';
-import 'package:flutter_web_ui/ui.dart' as ui;
+import 'package:flutter_ui/ui.dart' as ui;
 
 main() async {
   await ui.webOnlyInitializePlatform();
